72b9f7a96561fb55624580e6a03bf4e48e94be03
Carl Egonu <carlegonu@eggMac.local>
2019-04-24 10:45:39 +0100

    bubble sort

--- Theatre/src/com/carl/Main.java
+++ Theatre/src/com/carl/Main.java
@@ -1,16 +1,46 @@
 package com.carl;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
 
         Theatre Empire = new Theatre("Empire",9,12);
+        List<Theatre.Seat> EmpireCopy = new ArrayList<>(Empire.seats);
+
+        EmpireCopy.get(0).reserveSeat();
 
 //        Empire.getSeats();
         Empire.reserveSeat("A01");
         Empire.cancelSeat("A01");
         Empire.reserveSeat("B09");
         //System.out.println( Empire.getTheatreName());
+        printSeats(Empire.seats);
+        Collections.shuffle(Empire.seats);
+        printSeats(Empire.seats);
+        sortSeats(Empire.seats);
+        printSeats(Empire.seats);
+    }
+
+    public static void printSeats(List<Theatre.Seat> list){
+        for (Theatre.Seat seat: list) {
+            System.out.print(" " + seat.getSeatNumber());
+        }
+        System.out.println(" ");
+        System.out.println("=============================================");
+    }
+
+    public static void sortSeats(List<? extends Theatre.Seat> list){
+        for (int i=0; i<list.size()-1; i++){
+            for (int y=i+1; y<list.size(); y++){
+                if (list.get(i).compareTo(list.get(y))>0){
+                    Collections.swap(list,i,y);
+                }
+            }
+        }
     }
 }
--- Theatre/src/com/carl/Theatre.java
+++ Theatre/src/com/carl/Theatre.java
@@ -7,7 +7,7 @@ import java.util.List;
 
  public class Theatre {
     private final String theatreName;
-    private List<Seat> seats = new ArrayList<>();
+    public List<Seat> seats = new ArrayList<>();
 
     public Theatre(String theatreName, int numRows, int seatsPerRow) {
         this.theatreName = theatreName;
@@ -77,7 +77,7 @@ import java.util.List;
 
 
 
-    private class Seat implements Comparable<Seat>{
+    public class Seat implements Comparable<Seat>{
         private final String seatNumber;
         private boolean reserved;
 
@@ -113,7 +113,7 @@ import java.util.List;
             }
         }
 
-        private String getSeatNumber() {
+        public String getSeatNumber() {
             return seatNumber;
         }
     }
